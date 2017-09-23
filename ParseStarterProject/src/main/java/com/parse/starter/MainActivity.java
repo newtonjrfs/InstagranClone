8b7f1f3f73988ab18bb54c94727e88ccee4f8928
newtonjrfs <newtoljunior@hotmail.com>
2017-09-23 11:09:55 -0300

    Commit inicial criacao de tabela e insercao de dados usando back4app

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+      /*
+      ParseObject pontuacao = new ParseObject("Pontuacao");
+      pontuacao.put("nome","Jose");
+      pontuacao.put("pontos",8950);
+      pontuacao.saveInBackground(new SaveCallback() {
+          @Override
+          public void done(ParseException e) {
+
+              if(e==null){//nao temos erro
+                  Log.i("salvarPontos","Salvo com sucesso");
+              }else{
+                  Log.i("salvarPontos","Erro ao salvar pontos");
+              }
+
+          }
+      });
+      */
+
+
+
+      /*
+      ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
+      consulta.getInBackground("2VQcmPBk2b", new GetCallback<ParseObject>() {
+          @Override
+          public void done(ParseObject object, ParseException e) {
+              if(e==null){//nao temos erro
+                  object.put("pontos",9999);
+                  object.saveInBackground();
+              }else{
+                  Log.i("salvarPontos","Erro ao consultar objeto");
+              }
+          }
+      });
+       */
+
+      ParseQuery<ParseObject> filtro = ParseQuery.getQuery("Pontuacao");
+
+      filtro.whereGreaterThan("pontos",800);
+
+      filtro.findInBackground(new FindCallback<ParseObject>() {
+          @Override
+          public void done(List<ParseObject> objects, ParseException e) {
+
+              if(e==null){//nao temos erro
+                  for (ParseObject object : objects){
+                      Log.i("listarDados","Objeto - "+ object.get("nome") +" pontuacao : "+ object.get("pontos"));
+                  }
+              }else{
+                  Log.i("listarDados","Erro ao listar objetos - "+ e.getMessage());
+              }
+
+          }
+      });
+
+  }
+}
